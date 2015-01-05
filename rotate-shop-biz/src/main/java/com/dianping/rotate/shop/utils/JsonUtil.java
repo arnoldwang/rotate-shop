12dca7cc43b25d0b340d1965dc88a6a75d134d22
zhanghuagui <huagui.zhang>
2015-01-05 09:52:54 +0800

    remove test

---
+++ rotate-shop-biz/src/main/java/com/dianping/rotate/shop/utils/JsonUtil.java
@@ -0,0 +1,7 @@
+package com.dianping.rotate.shop.utils;
+
+/**
+ * Created by zaza on 15/1/5.
+ */
+public class JsonUtil {
+}
--- rotate-shop-biz/src/main/java/com/dianping/rotate/shop/utils/test.java
+++
@@ -1,7 +0,0 @@
-package com.dianping.rotate.shop.utils;
-
-/**
- * Created by zaza on 15/1/4.
- */
-public class test {
-}
