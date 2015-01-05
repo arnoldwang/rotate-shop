5e52516814ccd9e73eacd727cc9154c4dada2080
ming.luo <ming.luo@dianping.com>
2015-01-05 16:09:25 +0800

    shop region fixed

---
+++ rotate-shop-biz/src/main/java/com/dianping/rotate/shop/dao/ShopRegionDAO.java
@@ -0,0 +1,41 @@
+package com.dianping.rotate.shop.dao;
+
+import com.dianping.avatar.dao.GenericDao;
+import com.dianping.avatar.dao.annotation.DAOAction;
+import com.dianping.avatar.dao.annotation.DAOActionType;
+import com.dianping.avatar.dao.annotation.DAOParam;
+import com.dianping.rotate.shop.entity.RegionEntity;
+import com.dianping.rotate.shop.entity.ShopRegionEntity;
+
+import java.util.List;
+
+/**
+ * Created by luoming on 15/1/4.
+ */
+public interface ShopRegionDAO extends GenericDao {
+
+    @DAOAction(action = DAOActionType.QUERY)
+    List<ShopRegionEntity> queryShopRegionByRegionID(@DAOParam("regionID") int regionID);
+
+    @DAOAction(action = DAOActionType.QUERY)
+    List<ShopRegionEntity> queryShopRegionByShopID(@DAOParam("shopID") int shopID);
+
+    @DAOAction(action = DAOActionType.QUERY)
+    List<ShopRegionEntity> queryShopRegionByShopIDAndRegionID(@DAOParam("shopID") int shopID, @DAOParam("regionID") int regionID);
+
+    @DAOAction(action = DAOActionType.INSERT)
+    int addShopRegion(@DAOParam("shopRegion") ShopRegionEntity shopRegion);
+
+    @DAOAction(action = DAOActionType.UPDATE)
+    int deleteShopRegionByRegionID(@DAOParam("regionID") int regionID);
+
+    @DAOAction(action = DAOActionType.UPDATE)
+    int deleteShopRegionByShopID(@DAOParam("shopID") int shopID);
+
+    @DAOAction(action = DAOActionType.UPDATE)
+    int deleteShopRegionByShopIDAndRegionID(@DAOParam("shopID") int shopID, @DAOParam("regionID") int regionID);
+
+    @DAOAction(action = DAOActionType.UPDATE)
+    int updateShopRegion(@DAOParam("shopRegion") ShopRegionEntity shopRegion);
+
+}
---
+++ rotate-shop-biz/src/test/groovy/com/dianping/rotate/shop/dao/ShopRegionDAOTest.groovy
@@ -0,0 +1,42 @@
+package com.dianping.rotate.shop.dao
+
+import com.dianping.rotate.shop.AbstractSpockTest
+import com.dianping.rotate.shop.entity.RegionEntity
+import com.dianping.rotate.shop.entity.ShopRegionEntity
+import org.springframework.beans.factory.annotation.Autowired
+
+/**
+ * Created by luoming on 15/1/4.
+ */
+class ShopRegionDAOTest extends AbstractSpockTest {
+    @Autowired
+    ShopRegionDAO shopRegionDAO;
+
+    def "add, query and delete one region"() {
+        setup:
+        int shopID = 1
+        int regionID = 2
+
+        when:
+        def s = new ShopRegionEntity()
+        s.setShopID(shopID)
+        s.setRegionID(regionID)
+        s.setIsMain(1)
+        s.setStatus(1)
+        shopRegionDAO.addShopRegion(s)
+
+        then:
+        1 == shopRegionDAO.queryShopRegionByShopID(shopID).get(0).getShopID()
+        2 == shopRegionDAO.queryShopRegionByRegionID(regionID).get(0).getRegionID()
+        1 == shopRegionDAO.queryShopRegionByShopIDAndRegionID(1, 2).get(0).getIsMain()
+        s.setIsMain(0)
+        s.setId(shopRegionDAO.queryShopRegionByShopIDAndRegionID(1, 2).get(0).getId())
+        shopRegionDAO.updateShopRegion(s)
+        0 == shopRegionDAO.queryShopRegionByShopIDAndRegionID(1, 2).get(0).getIsMain()
+
+        cleanup:
+        shopRegionDAO.deleteShopRegionByRegionID(regionID)
+        shopRegionDAO.deleteShopRegionByShopID(shopID)
+        shopRegionDAO.deleteShopRegionByShopIDAndRegionID(shopID, regionID)
+    }
+}
