0fb8db340a1ecb679f2a87c7b822b1462c064893
zhanghuagui <huagui.zhang>
2015-01-06 09:55:21 +0800

    modify package path

---
+++ rotate-shop-mq/src/main/java/com/dianping/rotate/shop/factory/POIAddBatch.java
@@ -0,0 +1,29 @@
+package com.dianping.rotate.shop.factory;
+
+import com.dianping.rotate.shop.constants.MessageSource;
+import com.dianping.rotate.shop.constants.MessageStatus;
+import com.dianping.rotate.shop.constants.POIMessageType;
+import com.dianping.rotate.shop.dao.MessageQueueDAO;
+import com.dianping.rotate.shop.entity.MessageEntity;
+import com.dianping.swallow.common.message.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * Created by zaza on 15/1/5.
+ */
+public class POIAddBatch implements POIChange {
+    @Autowired
+    MessageQueueDAO messageQueueDAO;
+
+    @Override
+    public void addToMsgQueue(Message msg){
+        MessageEntity msgEntity = new MessageEntity();
+        msgEntity.setAttemptIndex(0);
+        msgEntity.setMsg(msg.getContent());
+        msgEntity.setSource(MessageSource.SYSTEM);
+        msgEntity.setStatus(MessageStatus.NEW);
+        msgEntity.setSwallowId(msg.getMessageId());
+        msgEntity.setType(POIMessageType.SHOP_ADD_BATCH);
+        messageQueueDAO.addToMessageQueue(msgEntity);
+    }
+}
