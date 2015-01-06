a790067c435a4442a768ddeb6d6fca25bb73d1f6
zhanghuagui <huagui.zhang>
2015-01-06 11:45:34 +0800

    add api package

--- rotate-shop-mq/src/main/java/com/dianping/roate/shop/factory/POIChange.java
+++
@@ -1,8 +0,0 @@
-package com.dianping.roate.shop.factory;
-import com.dianping.swallow.common.message.Message;
-/**
- * Created by zaza on 15/1/5.
- */
-public interface POIChange {
-    public void addToMsgQueue(Message msg);
-}
