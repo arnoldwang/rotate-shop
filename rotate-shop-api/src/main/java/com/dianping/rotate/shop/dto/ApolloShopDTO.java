6560025cd8dea3a096489da13df29106a7c0e95c
ming.luo <ming.luo@dianping.com>
2015-01-06 15:01:12 +0800

    apollo shop pigeon service

---
+++ rotate-shop-api/src/main/java/com/dianping/rotate/shop/dto/ApolloShopDTO.java
@@ -0,0 +1,116 @@
+package com.dianping.rotate.shop.dto;
+
+/**
+ * Created by luoming on 15/1/6.
+ */
+public class ApolloShopDTO {
+
+    private int shopID;
+    private int shopGroupID;
+    private int cityID;
+    private String district;
+    private int shopType;
+    private int shopStatus;
+    private int type;
+    private int bizID;
+    private String rating;
+    private int shopExtendStatus;
+    private int mainRegionID;
+    private int mainCategoryID;
+
+    public int getShopID() {
+        return shopID;
+    }
+
+    public void setShopID(int shopID) {
+        this.shopID = shopID;
+    }
+
+    public int getShopGroupID() {
+        return shopGroupID;
+    }
+
+    public void setShopGroupID(int shopGroupID) {
+        this.shopGroupID = shopGroupID;
+    }
+
+    public int getCityID() {
+        return cityID;
+    }
+
+    public void setCityID(int cityID) {
+        this.cityID = cityID;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public int getShopType() {
+        return shopType;
+    }
+
+    public void setShopType(int shopType) {
+        this.shopType = shopType;
+    }
+
+    public int getShopStatus() {
+        return shopStatus;
+    }
+
+    public void setShopStatus(int shopStatus) {
+        this.shopStatus = shopStatus;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public int getBizID() {
+        return bizID;
+    }
+
+    public void setBizID(int bizID) {
+        this.bizID = bizID;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public int getShopExtendStatus() {
+        return shopExtendStatus;
+    }
+
+    public void setShopExtendStatus(int shopExtendStatus) {
+        this.shopExtendStatus = shopExtendStatus;
+    }
+
+    public int getMainRegionID() {
+        return mainRegionID;
+    }
+
+    public void setMainRegionID(int mainRegionID) {
+        this.mainRegionID = mainRegionID;
+    }
+
+    public int getMainCategoryID() {
+        return mainCategoryID;
+    }
+
+    public void setMainCategoryID(int mainCategoryID) {
+        this.mainCategoryID = mainCategoryID;
+    }
+}
---
+++ rotate-shop-biz/src/main/java/com/dianping/rotate/shop/impl/ApolloShopServiceImpl.java
@@ -0,0 +1,84 @@
+package com.dianping.rotate.shop.impl;
+
+import com.dianping.rotate.shop.api.ApolloShopService;
+import com.dianping.rotate.shop.dao.ApolloShopDAO;
+import com.dianping.rotate.shop.dao.ApolloShopExtendDAO;
+import com.dianping.rotate.shop.dao.ShopCategoryDAO;
+import com.dianping.rotate.shop.dao.ShopRegionDAO;
+import com.dianping.rotate.shop.dto.ApolloShopDTO;
+import com.dianping.rotate.shop.entity.ApolloShopEntity;
+import com.dianping.rotate.shop.entity.ApolloShopExtendEntity;
+import com.dianping.rotate.shop.entity.ShopCategoryEntity;
+import com.dianping.rotate.shop.entity.ShopRegionEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+/**
+ * Created by luoming on 15/1/6.
+ */
+public class ApolloShopServiceImpl implements ApolloShopService {
+
+    @Autowired
+    private ApolloShopDAO apolloShopDAO;
+
+    @Autowired
+    private ApolloShopExtendDAO apolloShopExtendDAO;
+
+    @Autowired
+    private ShopRegionDAO shopRegionDAO;
+
+    @Autowired
+    private ShopCategoryDAO shopCategoryDAO;
+
+    @Override
+    public ApolloShopDTO getApolloShop(int shopID, int bizID) {
+        ApolloShopDTO apolloShopDTO = new ApolloShopDTO();
+        processShopExtend(apolloShopDTO, shopID, bizID);
+        processShop(apolloShopDTO, shopID);
+        processRegion(apolloShopDTO, shopID);
+        processCategory(apolloShopDTO, shopID);
+        return apolloShopDTO;
+    }
+
+    private void processShopExtend(ApolloShopDTO apolloShopDTO, int shopID, int bizID) {
+        List<ApolloShopExtendEntity> apolloShopExtendList = apolloShopExtendDAO.queryApolloShopExtendByShopIDAndBizID(shopID, bizID);
+        if(apolloShopExtendList != null && apolloShopExtendList.size() != 0) {
+            ApolloShopExtendEntity apolloShopExtend = apolloShopExtendList.get(0);
+            apolloShopDTO.setBizID(apolloShopExtend.getBizID());
+            apolloShopDTO.setType(apolloShopExtend.getType());
+            apolloShopDTO.setRating(apolloShopExtend.getRating());
+            apolloShopDTO.setShopExtendStatus(apolloShopExtend.getStatus());
+        }
+    }
+
+    private void processShop(ApolloShopDTO apolloShopDTO, int shopID) {
+        List<ApolloShopEntity> apolloShopList = apolloShopDAO.queryApolloShopByShopID(shopID);
+        if(apolloShopList != null && apolloShopList.size() != 0) {
+            ApolloShopEntity apolloShop = apolloShopList.get(0);
+            apolloShopDTO.setShopID(apolloShop.getShopID());
+            apolloShopDTO.setShopGroupID(apolloShop.getShopGroupID());
+            apolloShopDTO.setCityID(apolloShop.getCityID());
+            apolloShopDTO.setDistrict(apolloShop.getDistrict());
+            apolloShopDTO.setShopType(apolloShop.getShopType());
+            apolloShopDTO.setShopStatus(apolloShop.getStatus());
+        }
+    }
+
+    private void processRegion(ApolloShopDTO apolloShopDTO, int shopID) {
+        List<ShopRegionEntity> shopRegionList = shopRegionDAO.queryShopMainRegionByShopID(shopID);
+        if(shopRegionList != null && shopRegionList.size() != 0) {
+            ShopRegionEntity shopRegion = shopRegionList.get(0);
+            apolloShopDTO.setMainRegionID(shopRegion.getRegionID());
+        }
+    }
+
+    private void processCategory(ApolloShopDTO apolloShopDTO, int shopID) {
+        List<ShopCategoryEntity> shopCategoryList = shopCategoryDAO.queryShopMainCategoryByShopID(shopID);
+        if(shopCategoryList != null && shopCategoryList.size() != 0) {
+            ShopCategoryEntity shopCategory = shopCategoryList.get(0);
+            apolloShopDTO.setMainCategoryID(shopCategory.getCategoryID());
+        }
+    }
+
+}
