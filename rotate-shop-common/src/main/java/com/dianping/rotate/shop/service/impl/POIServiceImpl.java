5cb61ca2394358d3a651b8ab8c5be34c1f299bd6
Arnold Wang <zhenwei.wang@dianping.com>
2015-01-07 14:04:09 +0800

    poiservice

--- rotate-shop-common/src/main/java/com/dianping/rotate/shop/service/impl/POIServiceImpl.java
+++ rotate-shop-common/src/main/java/com/dianping/rotate/shop/service/impl/POIServiceImpl.java
@@ -1,5 +1,6 @@
 package com.dianping.rotate.shop.service.impl;
 
+import com.beust.jcommander.internal.Lists;
 import com.dianping.rotate.shop.entity.ApolloShopEntity;
 import com.dianping.rotate.shop.entity.ApolloShopExtendEntity;
 import com.dianping.rotate.shop.service.POIService;
@@ -8,7 +9,7 @@ import com.dianping.shopremote.remote.ShopService;
 import com.dianping.shopremote.remote.dto.ShopDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -16,6 +17,21 @@ import java.util.Map;
  * Date: 15-1-6
  */
 public class POIServiceImpl implements POIService{
+
+	public enum biz{
+		TUANTUI(1);
+
+		private int bizID;
+
+		biz(int bizID){
+			this.bizID = bizID;
+		}
+
+		public int getBizID(){
+			return bizID;
+		}
+
+	}
 
 	@Autowired
 	ShopService shopService;
@@ -27,7 +43,9 @@ public class POIServiceImpl implements POIService{
 			int shopId = Integer.valueOf((String)msgBody.get("shopId"));
 			ShopDTO shopDTO = shopService.loadShop(shopId);
 			ApolloShopEntity apolloShopEntity = createApolloShop(shopDTO);
-			
+			List<ApolloShopExtendEntity> apolloShopExtendList = Lists.newArrayList();
+			ApolloShopExtendEntity r = new ApolloShopExtendEntity();
+			r.setBizID(biz.TUANTUI.getBizID());
 
 		} catch (Exception e) {
 			//todo
@@ -38,7 +56,10 @@ public class POIServiceImpl implements POIService{
 		ApolloShopEntity apolloShopEntity = new ApolloShopEntity();
 		apolloShopEntity.setShopID(shopDTO.getShopId());
 		apolloShopEntity.setShopGroupID(shopDTO.getShopGroupId());
-		//todo
+		apolloShopEntity.setCityID(shopDTO.getCityId());
+		apolloShopEntity.setDistrict(shopDTO.getDistrict());
+		apolloShopEntity.setShopType(shopDTO.getShopType());
+		apolloShopEntity.setStatus(1);
 
 		return apolloShopEntity;
 	}
