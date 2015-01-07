99ea6552994b8bbee76958bc929eda904fc53bb2
Arnold Wang <zhenwei.wang@dianping.com>
2015-01-07 17:46:11 +0800

    add rotateGroupShopService

---
+++ rotate-shop-api/src/main/java/com/dianping/rotate/shop/api/RotateGroupShopService.java
@@ -0,0 +1,12 @@
+package com.dianping.rotate.shop.api;
+
+import com.dianping.rotate.shop.dto.RotateGroupShopDTO;
+
+/**
+ * User: zhenwei.wang
+ * Date: 15-1-7
+ */
+public interface RotateGroupShopService {
+
+	public RotateGroupShopDTO getRotateGroupShop(int rotateGroupID);
+}
