138581934509c76d90480a90476728e0a7e7140c
zhanghuagui <huagui.zhang>
2015-01-07 18:09:40 +0800

    add swallow consumer for poi

--- rotate-shop-mq/src/main/java/com/dianping/rotate/shop/factory/POIFactory.java
+++ rotate-shop-mq/src/main/java/com/dianping/rotate/shop/factory/POIFactory.java
@@ -1,32 +1,52 @@
 package com.dianping.rotate.shop.factory;
 import com.dianping.rotate.shop.constants.POIMessageType;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * Created by zaza on 15/1/5.
  */
 public class POIFactory {
+    @Autowired
+    POIAddBatch poiAddBatch;
+    @Autowired
+    POICategory poiCategory;
+    @Autowired
+    POIMerge poiMerge;
+    @Autowired
+    POIMergeRecover poiMergeRecover;
+    @Autowired
+    POIRegion poiRegion;
+    @Autowired
+    POIUpdate poiUpdate;
+    @Autowired
+    POIAdd poiAdd;
+    @Autowired
+    POIStatus poiStatus;
 
-    public static POIChange factory(int type){
+    public  POIChange factory(int type){
         if(type == POIMessageType.SHOP_ADD_BATCH){
-            return new POIAddBatch();
+            return poiAddBatch;
         }
         if(type == POIMessageType.SHOP_CATEGORY){
-            return new POICategory();
+            return poiCategory;
         }
         if(type == POIMessageType.SHOP_MERGE){
-            return new POIMerge();
+            return poiMerge;
         }
         if(type == POIMessageType.SHOP_MERGE_RECOVER){
-            return new POIMergeRecover();
+            return poiMergeRecover;
         }
         if(type == POIMessageType.SHOP_REGION){
-            return new POIRegion();
+            return poiRegion;
         }
         if(type == POIMessageType.SHOP_UPDATE){
-            return new POIUpdate();
+            return poiUpdate;
         }
         if(type == POIMessageType.SHOP_ADD){
-            return new POIAdd();
+            return poiAdd;
+        }
+        if(type == POIMessageType.SHOP_STATUS){
+            return poiStatus;
         }
         return null;
     }
--- rotate-shop-mq/src/main/java/com/dianping/rotate/shop/listener/POIShopStatusListener.java
+++ rotate-shop-mq/src/main/java/com/dianping/rotate/shop/listener/POIShopStatusListener.java
@@ -1,26 +1,24 @@
 package com.dianping.rotate.shop.listener;
 
+import com.dianping.rotate.shop.constants.POIMessageType;
 import com.dianping.rotate.shop.exception.POIMessageException;
 import com.dianping.rotate.shop.factory.POIChange;
 import com.dianping.rotate.shop.factory.POIFactory;
-import com.dianping.rotate.shop.service.POIChangeService;
 import com.dianping.swallow.common.message.Message;
 import com.dianping.swallow.consumer.MessageListener;
 import org.springframework.beans.factory.annotation.Autowired;
 
-
 /**
- * Created by zaza on 15/1/5.
+ * Created by zaza on 15/1/7.
  */
-public class POIChangeListener implements MessageListener {
+public class POIShopStatusListener implements MessageListener {
     @Autowired
     POIFactory poiFactory;
 
     @Override
-    public void onMessage(Message msg) throws POIMessageException{
-        int msgType = POIChangeService.getPOIChangeMessageType(msg);
-        POIChange poiChange = poiFactory.factory(msgType);
-        if(poiChange==null)return;
+    public void onMessage(Message msg) throws POIMessageException {
+        POIChange poiChange = poiFactory.factory(POIMessageType.SHOP_STATUS);
+        if(poiChange==null) return;
         poiChange.addToMsgQueue(msg);
     }
 }
