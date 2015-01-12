98eb24b43797fffef5d982ac195843d926d756d9
yangjie <hiyoungjay@gmail.com>
2015-01-12 19:39:20 +0800

    add invalid arguments test

--- rotate-shop-biz/src/test/groovy/com/dianping/rotate/shop/service/ApolloCategoryServiceTest.groovy
+++ rotate-shop-biz/src/test/groovy/com/dianping/rotate/shop/service/ApolloCategoryServiceTest.groovy
@@ -16,19 +16,40 @@ class ApolloCategoryServiceTest extends AbstractSpockTest {
     private final int c2 = 101;
     private final int c3 = 198;
 
-    private final int shanghai = 1;
+    private final int SHANGHAI = 1;
+
+    private final int INVALID_CITY_ID = -1;
+    private final int INVALID_C_ID = -1;
 
     def "get category"() {
         when:
-        CategoryDTO dto = categoryService.getCategory(c1, shanghai)
+        CategoryDTO dto = categoryService.getCategory(c1, SHANGHAI)
 
         then:
         dto.getCategoryID() == c1;
     }
 
+
+    def "get category with invalid city id"() {
+        when:
+        CategoryDTO dto = categoryService.getCategory(c1, INVALID_CITY_ID)
+
+        then:
+        dto.getCategoryID() == null;
+    }
+
+    def "get category with invalid category id"() {
+        when:
+        CategoryDTO dto = categoryService.getCategory(INVALID_C_ID, SHANGHAI)
+
+        then:
+        dto.getCategoryID() == null;
+    }
+
+
     def "get ancestors for c1"() {
         when:
-        List<CategoryDTO> dtos = categoryService.getCategoryAncestors(c1, shanghai);
+        List<CategoryDTO> dtos = categoryService.getCategoryAncestors(c1, SHANGHAI);
 
         then:
         dtos.size() == 1;
@@ -37,7 +58,7 @@ class ApolloCategoryServiceTest extends AbstractSpockTest {
 
     def "get ancestors for c2"() {
         when:
-        List<CategoryDTO> dtos = categoryService.getCategoryAncestors(c2, shanghai);
+        List<CategoryDTO> dtos = categoryService.getCategoryAncestors(c2, SHANGHAI);
 
         then:
         dtos.size() == 2;
@@ -46,9 +67,18 @@ class ApolloCategoryServiceTest extends AbstractSpockTest {
 
     def "get ancestors for c3"() {
         when:
-        List<CategoryDTO> dtos = categoryService.getCategoryAncestors(c3, shanghai);
+        List<CategoryDTO> dtos = categoryService.getCategoryAncestors(c3, SHANGHAI);
 
         then:
         dtos.size() == 3;
+    }
+
+
+    def "get ancestors for invalid category id"() {
+        when:
+        List<CategoryDTO> dtos = categoryService.getCategoryAncestors(INVALID_C_ID, SHANGHAI);
+
+        then:
+        dtos.size() == 0;
     }
 }
