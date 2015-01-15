1a916f4cb960476ac292fe451373c4d1ca98ec0b
yangjie <hiyoungjay@gmail.com>
2015-01-15 10:19:56 +0800

    add comment

--- rotate-shop-common/src/main/java/com/dianping/rotate/shop/service/ShopService.java
+++ rotate-shop-common/src/main/java/com/dianping/rotate/shop/service/ShopService.java
@@ -15,8 +15,8 @@ public interface ShopService {
 	// 更新轮转组的状态，包括是否单店，是否有效
 	// 逻辑是当轮转组有
 	// 0个门店 -> 无效
-	// 1个门店 -> 单店
-	// 2个及以上门店 -> 连锁店
+	// 1个门店 -> 有效 单店
+	// 2个及以上门店 -> 有效 连锁店
 	public void updateRotateGroupTypeAndStatusByRotateGroupId(int rotateGroupID);
 
 	/**
