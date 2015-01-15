5944b67a88e4982a2ee66cbd6f6c61c6a1715bd3
zhanghuagui <huagui.zhang>
2015-01-15 16:43:37 +0800

    add message producer

--- rotate-shop-job/src/main/java/com/dianping/rotate/shop/json/ShopMessage.java
+++ rotate-shop-job/src/main/java/com/dianping/rotate/shop/json/ShopMessage.java
@@ -3,9 +3,25 @@ package com.dianping.rotate.shop.json;
 /**
  * Created by zaza on 15/1/14.
  */
+
+/*
+  Topic：dp_apollo_shop_change
+  Json消息体
+  {
+    "shopId": 3454393,
+    "type":"insert"
+    }
+*/
 public class ShopMessage {
     private int shopId;
     private String type;
+
+    public ShopMessage(){}
+
+    public ShopMessage(int shopId,String type){
+        this.shopId = shopId;
+        this.type = type;
+    }
 
     public int getShopId() {
         return shopId;
