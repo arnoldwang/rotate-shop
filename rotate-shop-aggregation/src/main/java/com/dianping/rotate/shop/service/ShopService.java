85cfec2724d49e9684d508a976a79bb34d0338f2
Arnold Wang <zhenwei.wang@dianping.com>
2015-01-27 16:17:44 +0800

    add

--- rotate-shop-aggregation/src/main/java/com/dianping/rotate/shop/service/ShopService.java
+++ rotate-shop-aggregation/src/main/java/com/dianping/rotate/shop/service/ShopService.java
@@ -25,6 +25,16 @@ public interface ShopService {
 	 * @param rotateGroupID
 	 */
 	public void updateRotateGroupTypeAndStatusByRotateGroupId(int rotateGroupID);
+
+	/**
+	 * 优化后
+	 * 更新轮转组的状态，包括是否单店，是否有效
+	 * 逻辑是当轮转组有
+	 * 0个门店 -> 无效
+	 * 1个门店 -> 有效 单店
+	 * 2个及以上门店 -> 有效 连锁店
+	 * @param rotateGroupID
+	 */
 	public void updateRotateGroupTypeAndStatus(int rotateGroupID);
 
 	/**
--- rotate-shop-integration-job/src/main/java/com/dianping/rotate/shop/task/SyncShopExtendTypeWorkThread.java
+++ rotate-shop-integration-job/src/main/java/com/dianping/rotate/shop/task/SyncShopExtendTypeWorkThread.java
@@ -1,13 +1,27 @@
 package com.dianping.rotate.shop.task;
 
+import com.dianping.rotate.shop.service.ShopService;
+import org.apache.commons.collections.CollectionUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
 /**
  * User: zhenwei.wang
  * Date: 15-1-27
  */
 public class SyncShopExtendTypeWorkThread implements Runnable {
+	Logger logger = LoggerFactory.getLogger(SyncShopExtendTypeWorkThread.class);
+
+	private final static int PAGE_SIZE = 10000;
 
 	private int threadBegin;
 	private int threadEnd;
+
+	@Autowired
+	ShopService shopService;
 
 	public SyncShopExtendTypeWorkThread(int threadBegin, int threadEnd){
 		this.threadBegin = threadBegin;
@@ -21,5 +35,25 @@ public class SyncShopExtendTypeWorkThread implements Runnable {
 
 	private void syncShopExtendType(int threadBegin, int threadEnd) {
 		//todo
+		int exceptionFlag = 0;
+		int page = PAGE_SIZE;
+		int index = threadBegin;
+		while (index < (threadEnd + page) && exceptionFlag < 100) {
+			try {
+				List<Integer> rotateGroupIDList = shopService.getRotateGroupIDList(page, index);
+				if (CollectionUtils.isEmpty(rotateGroupIDList)) {
+					logger.info("This thread: " + Thread.currentThread().getName() + " rotateGroupID from " + index + " to " + (index + page) + " is empty!");
+					index += page;
+					continue;
+				}
+
+				for(int rotateGroupID: rotateGroupIDList){
+
+				}
+			}catch (Exception e){
+				logger.warn("This thread: " + Thread.currentThread().getName() + " sync data failed!", e);
+				exceptionFlag++;
+			}
+		}
 	}
 }
