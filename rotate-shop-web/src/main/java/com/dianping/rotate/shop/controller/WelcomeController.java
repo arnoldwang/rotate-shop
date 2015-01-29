b63755033dc78836870df84ce1ce86d66d6267e0
zhanghuagui <huagui.zhang>
2015-01-29 19:52:27 +0800

    add web config

---
+++ rotate-shop-web/src/main/java/com/dianping/rotate/shop/controller/WelcomeController.java
@@ -0,0 +1,7 @@
+package com.dianping.rotate.shop.controller;
+
+/**
+ * Created by zaza on 15/1/29.
+ */
+public class WelcomeController {
+}
---
+++ rotate-shop-web/src/main/java/com/dianping/rotate/shop/model/ModelTest.groovy
@@ -0,0 +1,7 @@
+package com.dianping.rotate.shop.model
+
+/**
+ * Created by zaza on 15/1/29.
+ */
+class ModelTest {
+}
