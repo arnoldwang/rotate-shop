7b82d61e70c627a2bec1bf5cdb1ae8c835bf7b28
yangjie <hiyoungjay@gmail.com>
2015-02-03 18:03:35 +0800

    add objectmapper

---
+++ rotate-shop-web/src/main/java/com/dianping/rotate/shop/factory/ObjectMapperFactory.java
@@ -0,0 +1,30 @@
+package com.dianping.rotate.shop.factory;
+
+/**
+ * Created by yangjie on 2/3/15.
+ */
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * Created by yangjie on 2/3/15.
+ */
+public class ObjectMapperFactory {
+
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+
+    static {
+        MAPPER.configure(JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES, true);
+        MAPPER.configure(JsonParser.Feature.ALLOW_UNQUOTED_CONTROL_CHARS, true);
+        MAPPER.configure(JsonParser.Feature.ALLOW_SINGLE_QUOTES, true);
+        MAPPER.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    }
+
+
+    public static ObjectMapper getMapper(){
+        return MAPPER;
+    }
+
+}
