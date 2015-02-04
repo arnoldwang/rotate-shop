438d40c4f97315076b0d6e75110b546b6c66403f
Arnold Wang <zhenwei.wang@dianping.com>
2015-02-04 16:04:06 +0800

    drop try catch

--- rotate-shop-web/src/main/java/com/dianping/rotate/shop/service/SwallowService.java
+++ rotate-shop-web/src/main/java/com/dianping/rotate/shop/service/SwallowService.java
@@ -4,8 +4,11 @@ import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * User: zhenwei.wang
@@ -18,8 +21,13 @@ public class SwallowService {
 	public Map<String, Object> createMsg(String msgType, String[] shopIDs) {
 		Map<String, Object> msgMap = Maps.newHashMap();
 		List<String> swallowMsgs = Lists.newArrayList();
+		List<String> correctShopIDs = new ArrayList<String>(shopIDs.length);
+		for(String shopID: shopIDs){
+			if(isNumber(shopID))
+				correctShopIDs.add(shopID);
+		}
 		if(msgType.equals("用户新增")){
-			for(String shopID: shopIDs){
+			for(String shopID: correctShopIDs){
 				StringBuilder swallowMsg = new StringBuilder("{'type':201,'pair':{'shopId':");
 				swallowMsg.append(shopID);
 				swallowMsg.append("}}");
@@ -28,7 +36,7 @@ public class SwallowService {
 			msgMap.put("topic", "dp_action");
 		}
 		if(msgType.equals("系统新增")){
-			for(String shopID: shopIDs){
+			for(String shopID: correctShopIDs){
 				StringBuilder swallowMsg = new StringBuilder("{'shopId':");
 				swallowMsg.append(shopID);
 				swallowMsg.append(",'messageType':4}");
@@ -37,7 +45,7 @@ public class SwallowService {
 			msgMap.put("topic", "dp_poi_change");
 		}
 		if(msgType.equals("POI变更")){
-			for(String shopID: shopIDs){
+			for(String shopID: correctShopIDs){
 				StringBuilder swallowMsg = new StringBuilder("{'shopID':");
 				swallowMsg.append(shopID);
 				swallowMsg.append(",'lasgModifyUser':-1234,'messageType':5}");
@@ -46,7 +54,7 @@ public class SwallowService {
 			msgMap.put("topic", "dp_poi_change");
 		}
 		if(msgType.equals("合并商户")){
-			for(String shopID: shopIDs){
+			for(String shopID: correctShopIDs){
 				StringBuilder swallowMsg = new StringBuilder("{'ShopID':");
 				swallowMsg.append(shopID);
 				swallowMsg.append(",'messageType':2}");
@@ -55,7 +63,7 @@ public class SwallowService {
 			msgMap.put("topic", "dp_poi_change");
 		}
 		if(msgType.equals("恢复商户")){
-			for(String shopID: shopIDs){
+			for(String shopID: correctShopIDs){
 				StringBuilder swallowMsg = new StringBuilder("{'RestoreShopID':");
 				swallowMsg.append(shopID);
 				swallowMsg.append(",'messageType':3}");
@@ -64,7 +72,7 @@ public class SwallowService {
 			msgMap.put("topic", "dp_poi_change");
 		}
 		if(msgType.equals("商户状态改变")){
-			for(String shopID: shopIDs){
+			for(String shopID: correctShopIDs){
 				StringBuilder swallowMsg = new StringBuilder("{'shopId':");
 				swallowMsg.append(shopID);
 				swallowMsg.append(",'lasgModifyUser':-1234}");
@@ -74,5 +82,14 @@ public class SwallowService {
 		}
 		msgMap.put("swallowMsgs", swallowMsgs);
 		return msgMap;
+	}
+
+	private boolean isNumber(String str) {
+		Pattern pattern = Pattern.compile("-?[0-9]+");
+		Matcher isNum = pattern.matcher(str);
+		if (!isNum.matches()) {
+			return false;
+		}
+		return true;
 	}
 }
