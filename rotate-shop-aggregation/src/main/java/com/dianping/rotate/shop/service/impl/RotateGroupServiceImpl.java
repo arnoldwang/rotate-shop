3567919501f357a2b31328af44ad97191f6a5090
ming.luo <ming.luo@dianping.com>
2015-02-06 11:31:26 +0800

    Task

---
+++ rotate-shop-aggregation/src/main/java/com/dianping/rotate/shop/service/impl/RotateGroupServiceImpl.java
@@ -0,0 +1,16 @@
+package com.dianping.rotate.shop.service.impl;
+
+
+import com.dianping.rotate.shop.service.RotateGroupService;
+
+/**
+ * Created by luoming on 15/2/6.
+ */
+public class RotateGroupServiceImpl implements RotateGroupService {
+
+    @Override
+    public boolean mergeRotateGroup() {
+        return false;
+    }
+
+}
