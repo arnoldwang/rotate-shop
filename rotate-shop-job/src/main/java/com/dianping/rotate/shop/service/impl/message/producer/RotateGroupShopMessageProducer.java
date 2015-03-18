4046a8be488c4311d77aac27e6db8a8bf6274ca7
zhanghuagui <huagui.zhang>
2015-03-18 17:21:15 +0800

    job bug fix

--- rotate-shop-job/src/main/java/com/dianping/rotate/shop/service/impl/message/producer/RotateGroupShopMessageProducer.java
+++ rotate-shop-job/src/main/java/com/dianping/rotate/shop/service/impl/message/producer/RotateGroupShopMessageProducer.java
@@ -1,14 +1,14 @@
 package com.dianping.rotate.shop.service.impl.message.producer;
 
-import com.dianping.pigeon.remoting.provider.config.annotation.Service;
 import com.dianping.rotate.shop.utils.JsonUtil;
+import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 
 /**
  * Created by zaza on 15/1/15.
  */
-@Service
+@Service("rotateGroupShopMessageProducerJob")
 public class RotateGroupShopMessageProducer extends AbstractMessageProducer {
     @Override
     public String getMessageJson(Object msg) throws IOException {
