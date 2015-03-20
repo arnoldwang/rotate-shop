e451d4bebe42dc58522fdcacf09f26f6af06ccd1
zhanghuagui <huagui.zhang>
2015-03-20 11:05:19 +0800

    增加了Swallow暂不接受消息的控制

--- rotate-shop-mq/src/main/java/com/dianping/rotate/shop/listener/POIAddListener.java
+++ rotate-shop-mq/src/main/java/com/dianping/rotate/shop/listener/POIAddListener.java
@@ -1,12 +1,11 @@
 package com.dianping.rotate.shop.listener;
 
 import com.dianping.rotate.shop.constants.POIMessageType;
-import com.dianping.rotate.shop.exception.POIMessageException;
 import com.dianping.rotate.shop.factory.POIChange;
 import com.dianping.rotate.shop.factory.POIFactory;
 import com.dianping.rotate.shop.service.POIChangeService;
 import com.dianping.rotate.shop.utils.Switch;
-import com.dianping.swallow.BackoutMessageException;
+import com.dianping.swallow.consumer.BackoutMessageException;
 import com.dianping.swallow.common.message.Message;
 import com.dianping.swallow.consumer.MessageListener;
 import org.slf4j.Logger;
@@ -23,16 +22,18 @@ public class POIAddListener implements MessageListener {
     private POIFactory poiFactory;
 
     @Override
-    public void onMessage(Message msg) throws POIMessageException{
-        try{
-            if(Switch.off()) return;
-            if(POIChangeService.getDPActionMessageType(msg)==DP_ACTION_ADD_TYPE){
+    public void onMessage(Message msg) throws BackoutMessageException{
+        try {
+            if (Switch.off()) return;
+            if (Switch.notAccept()) throw new BackoutMessageException("Not Accept Message");
+            if (POIChangeService.getDPActionMessageType(msg) == DP_ACTION_ADD_TYPE) {
                 POIChange poiChange = poiFactory.factory(POIMessageType.SHOP_ADD);
-                if(poiChange==null) return;
+                if (poiChange == null) return;
                 poiChange.addToMsgQueue(msg);
             }
         }catch(Exception ex){
             logger.error("Accept POI Add Message Error:"+ex.getMessage(),ex);
+            throw new BackoutMessageException("Accept POI Add Message Failed");
         }
     }
 }
--- rotate-shop-mq/src/main/java/com/dianping/rotate/shop/listener/POIChangeListener.java
+++ rotate-shop-mq/src/main/java/com/dianping/rotate/shop/listener/POIChangeListener.java
@@ -1,11 +1,11 @@
 package com.dianping.rotate.shop.listener;
 
-import com.dianping.rotate.shop.exception.POIMessageException;
 import com.dianping.rotate.shop.factory.POIChange;
 import com.dianping.rotate.shop.factory.POIFactory;
 import com.dianping.rotate.shop.service.POIChangeService;
 import com.dianping.rotate.shop.utils.Switch;
 import com.dianping.swallow.common.message.Message;
+import com.dianping.swallow.consumer.BackoutMessageException;
 import com.dianping.swallow.consumer.MessageListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,15 +21,17 @@ public class POIChangeListener implements MessageListener {
     private POIFactory poiFactory;
 
     @Override
-    public void onMessage(Message msg) throws POIMessageException{
+    public void onMessage(Message msg) throws BackoutMessageException{
         try{
             if(Switch.off()) return;
+            if (Switch.notAccept()) throw new BackoutMessageException("Not Accept Message");
             int msgType = POIChangeService.getPOIChangeMessageType(msg);
             POIChange poiChange = poiFactory.factory(msgType);
             if(poiChange==null)return;
             poiChange.addToMsgQueue(msg);
         }catch(Exception ex){
             logger.error("Accept POI Change Message Error:"+ex.getMessage(),ex);
+            throw new BackoutMessageException("Accept POI Change Message Failed");
         }
 
     }
